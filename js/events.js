68d79398f5f3fa451574f10e4448bc13e418811c
Nina Sweeney <nsweeney@middlebury.edu>
2018-06-27 20:58:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,10 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown',function(key) {
-    if (key.which ===)
-  }
+    if (key.which ===71){
+      alert('g was pressed');
+    }
+  });
 }
 
 
